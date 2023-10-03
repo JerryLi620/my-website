6075ecb556923f8e571dc39be7b5c6ccf4c61702
Jerry Li <lmyjerry@gmail.com>
2023-10-02 20:07:59 -0400

    research

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -70,22 +70,24 @@ const repos = {
   ],
 };
 
-// Leadership SECTION
+// Research SECTION
 const leadership = {
-  show: false,
-  heading: "Leadership",
+  show: true,
+  heading: "Research",
   message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+    "Here are the posters about my researches during my undergraduate years.",
   images: [
     {
-      img: require("../editable-stuff/mingyangli.jpg"),
-      label: "First slide label",
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      img: require("../editable-stuff/unpair.jpg"),
+      label:
+        "Unpaired Pointcloud Translation using Diffusion Probablistic Model",
+      paragraph: "",
     },
     {
-      img: require("../editable-stuff/mingyangli.jpg"),
-      label: "Second slide label",
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      img: require("../editable-stuff/pointcloud.jpg"),
+      label:
+        "Point Cloud Based Machine Learning for Analysis of Nuclear Reactions",
+      paragraph: "",
     },
   ],
   imageSize: {
